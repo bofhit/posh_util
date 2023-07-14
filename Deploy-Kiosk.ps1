d4dcf0f7e8ff5149533e3e2f3eecb0320cfedc57
bofhit <90052881+bofhit@users.noreply.github.com>
2023-07-14 13:50:38 -0400

    Create Deploy-Kiosk.ps1
    Initial commit.

---
+++ Deploy-Kiosk.ps1
@@ -0,0 +1,53 @@
+<#
+.SYNOPSIS
+    Set up a PC for kiosk use.
+.DESCRIPTION
+    Set the baseline config for a general purpose kiosk PC.
+#>
+
+$ADMIN_ACCOUNT = 'admiot'
+
+# ============================================================================
+# Pre-flight checks.
+
+# Check for elevation.
+$id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object System.Security.Principal.WindowsPrincipal($id)
+
+if (-not($principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))){
+    throw 'Please run this script as an administrator.'
+}
+
+# Check that script execution is enabled.
+$executionPolicy = Get-ExecutionPolicy
+if (-not($executionPolicy -eq 'RemoteSigned')) {
+    throw 'Please set Execution-Policy to RemoteSigned'
+}
+
+# ============================================================================
+
+# Set-Timezone
+Set-TimeZone -Id 'Eastern Standard Time'
+
+# Add admin account.
+$password = Read-Host -AsSecureString
+New-LocalUser $ADMIN_ACCOUNT -FullName 'IT Admin' -Password $password
+Add-LocalGroupMember -Group administrators -Member $ADMIN_ACCOUNT
+
+# Remove all other accounts.
+Get-LocalUser | ForEach-Object {if (-not($_.Name -eq $ADMIN_ACCOUNT)){Remove-LocalUser $_.name}}
+
+# Enable Remote Desktop
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+# Set power options, timeout of 0 -eq Never
+powercfg.exe -x -monitor-timeout-ac 0
+powercfg.exe -x -monitor-timeout-dc 0
+powercfg.exe -x -disk-timeout-ac 0
+powercfg.exe -x -disk-timeout-dc 0
+powercfg.exe -x -standby-timeout-ac 0
+powercfg.exe -x -standby-timeout-dc 0
+powercfg.exe -x -hibernate-timeout-ac 0
+powercfg.exe -x -hibernate-timeout-dc 0
+
